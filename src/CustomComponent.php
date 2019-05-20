06c774e393e4a3f5022d780d3394036a9d4ace8a
Patrick Stahl <p.stahl@shopware.com>
2019-05-20 14:01:34 +0200

    Initial commit

---
+++ src/CustomComponent.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Swag\CustomComponent;
+
+use Shopware\Core\Framework\Plugin;
+
+class CustomComponent extends Plugin
+{
+}
---
+++ src/Resources/administration/app/component/custom-component/hello-world/index.js
@@ -0,0 +1,5 @@
+import { Component } from 'src/core/shopware';
+
+Component.register('hello-world', {
+    template: '<h2>Hello world!</h2>'
+});
